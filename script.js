80d2023f0c5c242062775d29a208a3089d64f569
Dmitriy <devilins24@gmail.com>
2022-01-25 22:24:54 +0200

    add accessibility, styles adjustment, tablet style

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const navIcon = document.querySelectorAll(".nav__menu--icon");
 const navMenu = document.querySelector(".nav__menu-container");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const nav = document.querySelector(".primary-navigation");
 
-[...navIcon].map((icon) => {
-  icon.addEventListener("click", () => {
-    navMenu.classList.toggle("show");
-  });
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+  const visiblity = nav.getAttribute("data-visible");
+  if (visiblity === "false") {
+    nav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else {
+    nav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+  //   console.log(nav.getAttribute("data-visible"));
 });
