db0d7a50d7d5b295dd5f5efceb417191bf43cee9
Dmitriy <devilins24@gmail.com>
2022-01-29 11:53:38 +0200

    add destination page tab list js func, content toggle

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Nav menu
 const navMenu = document.querySelector(".nav__menu-container");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const nav = document.querySelector(".primary-navigation");
@@ -12,4 +13,35 @@ navToggle.addEventListener("click", () => {
     nav.setAttribute("data-visible", false);
     navToggle.setAttribute("aria-expanded", false);
   }
+});
+
+// Tab list - change showed content on click
+const tabs = document.querySelectorAll('[role="tab"]');
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (e) => {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
+
+    // info panel update
+    const tabContainer = tab.parentNode;
+    const mainContainer = tabContainer.parentNode;
+    targetingContent = tab.getAttribute("aria-controls");
+
+    mainContainer.querySelectorAll("article").forEach((article) => {
+      article.setAttribute("hidden", true);
+    });
+    mainContainer.querySelector(`#${targetingContent}`).removeAttribute("hidden");
+
+    // picture panel update
+    const pictureContainer = document.querySelector(".left-destination-section");
+    const targetImage = tab.getAttribute("data-image");
+
+    pictureContainer.querySelectorAll("picture").forEach((picture) => {
+      picture.setAttribute("hidden", true);
+    });
+    pictureContainer.querySelector(`#${targetImage}`).removeAttribute("hidden");
+  });
 });
